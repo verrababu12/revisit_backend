ef5bac172a08ee36d7eca0a3f8bb5b55d1a10c72
veeru <veeru@gmail.com>
2025-04-12 17:48:50 +0530

    urlupdated

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    credentials: true, 
+    origin: ["https://revisit-frotend.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
